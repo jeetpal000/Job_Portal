fae4687cf8aa43bec3deb2a68f250d3df2b09b76
JEET <chandrajeetcare2019@gmail.com>
2026-02-20 17:21:34 +0530

    failed compile error fix

--- src/components/pages/jobPostorm/jobAction.ts
+++ src/components/pages/jobPostorm/jobAction.ts
@@ -5,21 +5,17 @@ import { JobPostTable } from "@/src/model/schema";
 import { CreateServer } from "@/src/utils/db";
 import { jobPostSchemaData } from "@/src/validation/auth.validation"
 
-export const jobPostAction = async(data: jobPostSchemaData, id: string | null)=>{
+export const jobPostAction = async(data: jobPostSchemaData, id: string | null):Promise<{message: string}> =>{
     const {
         title, jobType, workType, jobLevel, location, tags, minSalary, maxSalary, currency, period, minEducation, date, experience, description
     } = data;
     await CreateServer();
-    const jobTable = await JobPostTable.findByIdAndUpdate(id,
-        // {
-        //     title, jobType, workType, jobLevel, location, tags, minSalary, maxSalary, currency, period, minEducation, date, experience, description,
-        // }
-        data,
-        {new: true}
-    )
-    console.log(id);
-    if(jobTable){
-            return {message: "Job updated successfully"}
+    if(id){
+
+        const jobTable = await JobPostTable.findByIdAndUpdate(id, data, {new: true})
+            if(jobTable){
+                return {message: "Job updated successfully"}
+            }
     }
 
         const user = await getCurrentuser();      
